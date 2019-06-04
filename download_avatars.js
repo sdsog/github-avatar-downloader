a12deaf6bfc487f4ad0b9d7ee7e7a4906e279fe9
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-04 02:55:23 +0000

    added download avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,10 @@
 var authTok = require('./secrets');
 var request = require('request');
 var fs = require('fs');
+var args = process.argv.slice(2);
+
+// console.log(args[0]);
+// console.log(args[1]);
 
 
 function getRepoContributors(repoOwner, repoName, cb) { 
@@ -11,21 +15,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': authTok.GITHUB_TOKEN
     }
   };
+
   request(options, function(err, res, body) {
+  	if (!repoName) {
+  		console.log("Must enter valid repo name.");
+  	} else if (!repoOwner) {
+  		console.log("Must enter valid repo owner.");
+  	}else{
     cb(err, body);
+	}
+
   });
 }
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  var temp = JSON.parse(result);
-  temp.forEach(function(element) {
-  downloadImageByURL(element.avatar_url, 'avatars/' + element.login + '.jpg');
-  });
-});
-
-
 
 function downloadImageByURL(url, filePath) {
 	request.get(url)
@@ -34,31 +35,13 @@ function downloadImageByURL(url, filePath) {
 
 
 
+getRepoContributors(args[0], args[1], function(err, result) {
+  var temp = JSON.parse(result);
+  temp.forEach(function(element) {
+  downloadImageByURL(element.avatar_url, 'avatars/' + element.login + '.jpg');
+  });
+});
 
-// request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-//        .on('error', function (err) {                                   // Note 2
-//          throw err; 
-//        })
-//        .on('response', function (response) {                           // Note 3
-//          console.log('Status Message: ', response.statusMessage);
-//          console.log('Response Headers: ', response.headers['content-type']);
-//        })
-//        .pipe(fs.createWriteStream('./future.jpg'));   
-
-
-
-// headers: {
-//     'user-agent': 'node application'
-// }
-
-
-
-// Your next and final step in this exercise should be to change your getRepoContributors
-// function to parse the JSON string into an object and pass this object
-// (an array of contributor objects) to the cb function.
-
-// You will also need to modify the callback function to iterate over the results
-// and (for now) console.log the value for each avatar_url in the collection:
 
 
 
