7330a9a9452d64c3865ec0c45702abc4ab58e108
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-04 01:24:50 +0000

    receiving url path

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,52 @@
+var authTok = require('./secrets');
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) { // ...
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'node application',
+      'Authorization': authTok.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+
+  });
+
+}
+
+//console.log('Welcome to the GitHub Avatar Downloader!');
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  var temp = JSON.parse(result);
+  //console.log("Result:", temp);
+  temp.forEach(function(element){
+  	console.log(element.avatar_url);
+  });
+});
+
+
+function downloadImageByURL(url, filePath) {
+  // ...
+}
+
+// headers: {
+//     'user-agent': 'node application'
+// }
+
+/*
+
+Your next and final step in this exercise should be to change your getRepoContributors
+function to parse the JSON string into an object and pass this object
+(an array of contributor objects) to the cb function.
+
+You will also need to modify the callback function to iterate over the results
+and (for now) console.log the value for each avatar_url in the collection:
+
+*/
+
